ccd9879ac135a12c2d0fcb3157aa44fd452b8fb8
matiaschiappinelli <matppinelli@gmail.com>
2020-08-25 19:04:49 -0300

    agrego carpeta para scripts de prueba de fortran

---
+++ Tutorial_Fortran/program ejemplo1.f90
@@ -0,0 +1,19 @@
+program ejemplo1.f90
+
+      implicit none
+      integer:: a,b,c
+      real:: e,f,g
+
+      a=10
+      b=20
+
+      c=a+b
+
+      e=10.22
+      f=8.79
+      g=e+f
+
+      print *, 'Las sumas son: ',c,g
+      pause
+
+      end program
